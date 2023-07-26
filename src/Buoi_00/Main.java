5c104047db932353e1c87058418287e1793aa752
Vincent <giaduy218@gmail.com>
2023-07-26 20:48:37 +0700

    Vincent: Guide to importing the data

--- src/Buoi_00/Main.java
+++ src/Buoi_00/Main.java
@@ -1,5 +1,7 @@
 package Buoi_00;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to CodeGym - Automaton Test Course\n");
@@ -7,5 +9,16 @@ public class Main {
         System.out.println("***************** 1991 ****************");
         System.out.println("***** HV CN Buu chinh Vien thong ******");
         System.out.println("**** Tro thanh Automation Engineer ****");
+
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input your string: ");
+        String value = sc.nextLine();
+        System.out.println("Your string: " + value);
+
+        System.out.print("Input your number: ");
+        int number = sc.nextInt();
+        System.out.println("Your number : "+ number);
     }
 }
--- src/Buoi_12/Main.java
+++ src/Buoi_12/Main.java
@@ -1,5 +1,7 @@
 package Buoi_12;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
