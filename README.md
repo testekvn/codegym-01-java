677da4d44b5910b6cd6330688f66e84a43a36885
Vincent <giaduy218@gmail.com>
2023-07-26 20:36:11 +0700

    Vincent: Init Project - CodeGym - 01

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# codegym-01-java
+# The Automation Test Course - CodeGym
+Coach: Vincent - 0832868822
+
+Agenda:
+1. Java Basic
+2. Automation Frameworks
+   1. Selenium 
+   2. TestNG
+   3. Extent Report
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to CodeGym - Automaton Test Course\n");
+        System.out.println("*************** Vincent ***************");
+        System.out.println("***************** 1991 ****************");
+        System.out.println("***** HV CN Buu chinh Vien thong ******");
+        System.out.println("**** Tro thanh Automation Engineer ****");
+    }
+}
