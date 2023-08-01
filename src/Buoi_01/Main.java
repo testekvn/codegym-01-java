30bdb46410a0d10baee526a566099a1b7848ce0d
vincent <giaduy218@gmail.com>
2023-08-01 16:36:27 +0700

    Vincent: Add for lession 1

--- src/Buoi_01/Main.java
+++ src/Buoi_01/Main.java
@@ -3,5 +3,40 @@ package Buoi_01;
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
+
+        // region Project Hello World
+
+        // endregion
+
+        // region Variable : Biến trong Java
+
+        // region Init Variable
+
+        // endregion
+
+        // region Toán tử
+
+        // endregion
+
+        // region If - Else : Cấu trúc điều kiện
+
+        // endregion
+
+        // region If - Else lồng : Cấu trúc điều kiện lồng
+
+        // endregion
+
+        // region If - Else bậc thang : Cấu trúc điều kiện bậc thang
+
+        // endregion
+
+
+        // region Switch case : Cấu trúc điều kiện
+
+        // endregion
+
+
+
+        // endregion
     }
 }
