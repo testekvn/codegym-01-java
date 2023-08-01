ad963be78086b081a85c7105441ae5c06ecf2085
phamminh13092001 <119114251+phamminh13092001@users.noreply.github.com>
2023-08-01 21:35:38 +0700

    hello

--- src/Buoi_01/Main.java
+++ src/Buoi_01/Main.java
@@ -3,5 +3,42 @@ package Buoi_01;
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
+        //region
+
+        //enregion
+        System.out.println("helloworld");
+        int index = 10; // khai bao bien index va gan gia tri
+        int index2;
+        /**
+         *
+         */
+        index2 = 10;
+        int a=10, b=11, c=12;
+        //region Const
+
+        //endregion
+        //region bien
+        byte tuoi = 100;
+        short nam = 2023;
+        int tien = 100000;
+        int g = Integer.MAX_VALUE;
+        float d =1.5f;
+        long e = 1000000000;
+        boolean f = true;
+        System.out.println("" +tuoi);
+        System.out.printf("%s" ,tuoi);
+        int n = 13;
+        if(n%2==0){
+
+        }
+        String m ="so le";
+        if(n%2==0){
+            m = "so chan";
+        }
+        System.out.println(m +n);
+        //endregion
+        //region dieu kien
+
+        //endregion
     }
 }
