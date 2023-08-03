b85120aaca11d86cfd06e0598bbae6f4971c18c1
unknown <zmidouzkage96hy@gmail.com.com>
2023-08-03 20:08:33 +0700

    quyetpv test git2

--- src/Buoi_02/Main.java
+++ src/Buoi_02/Main.java
@@ -3,5 +3,8 @@ package Buoi_02;
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
+        //region test git2
+
+        //endregion
     }
 }
