e944ab228d4d8f7e6859d02ea14f5f2a0e7ff185
phungxtnd <phungdangxtnd@gmail.com>
2023-08-03 20:04:59 +0700

    abcd

---
+++ src/Buoi_00/Helloword.java
@@ -0,0 +1,12 @@
+package Buoi_00;
+
+public class Helloword {
+    public static void main (String[] args){
+
+        int index=10;
+        int indexs;
+        indexs =11;
+        System.out.println("Helloword");
+
+    }
+}
--- src/Buoi_02/Main.java
+++ src/Buoi_02/Main.java
@@ -3,5 +3,6 @@ package Buoi_02;
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
+        //BTVN
     }
 }
