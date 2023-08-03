17954b426c0659df2b7dec622cbc957dfc218105
vincent <giaduy218@gmail.com>
2023-08-03 17:51:58 +0700

    Vincent - Feature: Add new regions for lession 02

--- src/Buoi_02/Main.java
+++ src/Buoi_02/Main.java
@@ -3,5 +3,31 @@ package Buoi_02;
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
+
+        //region for
+
+        //endregion
+
+        //region while
+
+        //endregion
+
+        //region do-while
+
+        //endregion
+
+        //region for-each
+
+        //endregion
+
+        //region break
+
+        //endregion
+
+        //region continue
+
+        //endregion
+
+
     }
 }
