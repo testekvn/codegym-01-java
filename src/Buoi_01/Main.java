7625e33c99f7a62d8296f4898ec0bca4a7fac32f
quyetpv <zmidouzkage96hy@gmail.com.com>
2023-08-03 20:27:43 +0700

    quyetpv update buoi1

--- src/Buoi_01/Main.java
+++ src/Buoi_01/Main.java
@@ -3,8 +3,8 @@ package Buoi_01;
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
-        //region tes1
-        System.out.println("test1");
-        //endregion
+        //region test2
+
+        //test3
     }
 }
