adb75e2e4ee6f3b6649af9c48dd905bbe569859d
quyetpv <zmidouzkage96hy@gmail.com.com>
2023-08-03 20:19:00 +0700

    quyetpv update test3

--- src/Buoi_01/Main.java
+++ src/Buoi_01/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
         //region tes1
-
+        System.out.println("test1");
         //endregion
     }
 }
