c325ef7d1f0308eaaf023cbf2cb26ca147e2c396
phamminh13092001 <119114251+phamminh13092001@users.noreply.github.com>
2023-08-03 19:54:07 +0700

    minh

---
+++ src/Buoi_01/Bai1.java
@@ -0,0 +1,23 @@
+package Buoi_01;
+
+public class Bai1 {
+    public class Test_bien {
+        public static void main(String[] arg) {
+            int a = 10;
+            String b = "Phạm Quang Minh";
+            float c = 1.5f;
+            char d = 'm';
+            boolean e = true;
+            double f = 10.5;
+            System.out.println("a=" +a);
+            System.out.println("b=" +b);
+            System.out.println("c=" +c);
+            System.out.println("d=" +d);
+            System.out.println("e=" +e);
+            System.out.println("f=" +f);
+        }
+        //test
+    }
+
+
+}
