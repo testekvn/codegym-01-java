00171c8a27cb3b6cc01a7569e9d66182fc1a0c7f
quyetpv <zmidouzkage96hy@gmail.com>
2023-08-05 01:29:34 +0700

    quyetpv: add test code at home

--- src/Buoi_02/Main.java
+++ src/Buoi_02/Main.java
@@ -3,8 +3,8 @@ package Buoi_02;
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
-        //region test git2
-
+        //region add cmd test git at home
+        System.out.println("Bai tap so 2");
         //endregion
     }
 }
