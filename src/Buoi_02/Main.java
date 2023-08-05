7776cec79c4abfa438044e99c6b14a2ec55d0649
quyetpv <zmidouzkage96hy@gmail.com>
2023-08-06 03:48:50 +0700

    quyetpv: Nop bai tap buoi 2

--- src/Buoi_02/Main.java
+++ src/Buoi_02/Main.java
@@ -1,10 +1,121 @@
 package Buoi_02;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
-        //region add cmd test git at home
-        System.out.println("Bai tap so 2");
+        //region Bai1
+        System.out.println("This is menu to draw");
+        System.out.println("1. Print the rectangle");
+        System.out.println("2. Print the square triangle (The corner is square at 4 different angles: top-left, top-right, botton-left, botton-right)");
+        System.out.println("3. Print isosceles triangle");
+        System.out.println("4. Exit");
+        System.out.print("Please click the number to draw picture or exit: ");
+
+        while (true){
+            Scanner sc = new Scanner(System.in);
+            int option = sc.nextInt();
+            if (option == 4) break;
+            switch (option){
+                case 1:
+                    System.out.println("The rectangle(10x3):");
+                    for (int i = 0; i < 3; i++){
+                        for (int j = 0; j < 10; j++){
+                            System.out.print("*");
+                        }
+                        System.out.println(" ");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Print the square triangle:");
+                    System.out.println("The corner: top-left:");
+                    for (int i = 5; i > 0; i--){
+                        for (int j = 0; j < i; j++){
+                            System.out.print("*");
+                        }
+                        System.out.println(" ");
+                    }
+                    System.out.println("The corner: top-right:");
+                    for (int i = 0; i < 5; i++){
+                        for (int j = 0; j < i; j++){
+                            System.out.print(" ");
+                        }
+                        for (int j = i; j < 5; j++){
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("The corner: botton-left:");
+                    for (int i = 0; i < 5; i++){
+                        for (int j = 0; j <= i; j++){
+                            System.out.print("*");
+                        }
+                        System.out.println(" ");
+                    }
+                    System.out.println("The corner: botton-right:");
+                    for (int i = 5; i > 0; i--){
+                        for (int j = 0; j < i; j++){
+                            System.out.print(" ");
+                        }
+                        for (int j = 0; j < 5 - i +1; j++){
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Isosceles triangle: ");
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 7; j++) {
+                            if (i == 0|| j == i || j == 7 - i - 1) {
+                                System.out.print("* ");
+                            } else System.out.print("  ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                default:
+                    System.out.println("Error, please try again");
+            }
+            System.out.print("Please click the number to draw picture or exit: ");;
+        }
+        //endregion
+        //region Bai2
+        System.out.print("Số lượng số nguyên tố cần in ra là: ");
+        Scanner sc = new Scanner(System.in);
+        int numbers = sc.nextInt();
+        int count = 0;
+        int N = 2;
+        while (count < numbers){
+            if (songuyento(N)){
+                System.out.print(N+ " ");
+                count++;
+            }
+            N++;
+        }
+        System.out.println();
+        //endregion
+        //region Bai3
+        System.out.println("Các số nguyên tố nhỏ hơn 100 là:");
+        for (int i =2; i < 100; i++){
+            if (songuyento(i)){
+                System.out.print(i+" ");
+            }
+        }
         //endregion
     }
+    //region kiem tra so nguyen to
+    public static boolean songuyento(int num){
+        if (num < 2){
+            return false;
+        }
+        for (int i =2; i <= Math.sqrt(num); i++){
+            if (num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    //endregion kiem tra so nguyen to
 }
