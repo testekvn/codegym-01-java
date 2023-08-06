3978eac5624fc2ac03856e995760f90067321ffb
= <=>
2023-08-06 20:59:55 +0700

    Bap tap 01/ Buoi 01

---
+++ src/Buoi_01/BaiTap01.java
@@ -0,0 +1,13 @@
+package Buoi_01;
+
+import java.util.Scanner;
+
+public class BaiTap01 {
+    public static void main(String[] args) {
+        System.out.println("Bai 01");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap loi chao: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
