d2986e42377f15f1954ec35cbed8f3de42c602ed
= <=>
2023-08-06 20:34:31 +0700

    Bai tap 03

---
+++ src/Buoi_02/BaiTap02.java
@@ -0,0 +1,8 @@
+package Buoi_02;
+
+public class BaiTap02 {
+    public static void main(String[] args) {
+        System.out.println("bai 2");
+
+    }
+}
---
+++ src/Buoi_02/Baitap0333.java
@@ -0,0 +1,14 @@
+package Buoi_02;
+
+public class Baitap0333 {
+    public static void main(String[] args) {
+        System.out.println("Bai 3");
+        for (int i = 2; i < 100; i++) {
+            boolean check = true;
+            for (int j = 2; j < Math.sqrt(i); j++) {
+                if (i % j == 0) check = false;
+            }
+            if (check) System.out.print(i + " ");
+        }
+    }
+}
