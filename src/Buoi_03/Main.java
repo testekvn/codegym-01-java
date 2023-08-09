f39fefdded7eabd04b921c083c4579383d6e5de8
Dung Le <you@example.com>
2023-08-10 00:36:18 +0700

    bai tap buoi 3

--- src/Buoi_03/Main.java
+++ src/Buoi_03/Main.java
@@ -3,5 +3,24 @@ package Buoi_03;
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
+        //region bai tap
+        int[] arrInt;
+        arrInt = new int[50];
+        int count = 0;
+        for (int i = 10; i < 201; i++) {
+            if (i % 2 == 0) {
+  //              System.out.println("So Chan: " + i);
+                arrInt[count] = i;
+                count++;
+            }
+
+            if (count == 50) break;
+
+            }
+        for (int i = 0; i < arrInt.length; i++) {
+           if (arrInt[i] == 68)
+               System.out.println("Vi tri: "+(i+1)); //cong them 1 neu la nguoi dung
+            //endregion
+        }
     }
 }
---
+++ src/Buoi_03/baiTap2_b3.java
@@ -0,0 +1,30 @@
+package Buoi_03;
+
+public class baiTap2_b3 {
+    public static void main(String[] args) {
+        int[] arrNum;
+        arrNum= new int[6];
+        for(int i=1;i<10;i++){
+            for(int j=0;j<10;j++){
+                for(int k=0;k<10;k++){
+                    for(int n=0;n<10;n++){
+                        for(int m=0;m<10;m++){
+                            for(int l=1;l<10;l++){
+                                arrNum[0]=i;
+                                arrNum[1]=j;
+                                arrNum[2]=k;
+                                arrNum[3]=n;
+                                arrNum[4]=m;
+                                arrNum[5]=l;
+                                if(arrNum[0]==arrNum[5] && arrNum[1]==arrNum[4] && arrNum[2]==arrNum[3] ){
+                                    System.out.println(" "+arrNum[0]+arrNum[1]+arrNum[2]+arrNum[3]+arrNum[4]+arrNum[5]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
