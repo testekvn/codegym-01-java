671fe21f27a29766a96d34d4ce6733f5b675c3dc
vananh <vananhgit@gmail.com>
2023-08-09 21:33:58 +0700

    Buoi 3

--- src/Buoi_01/BaiTap03.java
+++ src/Buoi_01/BaiTap03.java
@@ -4,11 +4,8 @@ import java.util.Scanner;
 
 public class BaiTap03 {
     public static void main(String[] args) {
-        double chieucao =0 ;
         double cannang = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap chieu cao: ");
-        chieucao = sc.nextDouble();
         System.out.println("Nhap can nang: ");
         cannang = sc.nextDouble();
 
@@ -18,107 +15,110 @@ public class BaiTap03 {
         String hangChuc;
         int hangTram01 = 0;
         String hangTram;
-        switch (donVi){
-            case 1:
-                hangDonVi = "one";
-                break;
-            case 2:
-                hangDonVi = "two";
-                break;
-            case 3:
-                hangDonVi = "three";
-                break;
-            case 4:
-                hangDonVi = "four";
-                break;
-            case 5:
-                hangDonVi = "five";
-                break;
-            case 6:
-                hangDonVi = "six";
-                break;
-            case 7:
-                hangDonVi = "seven";
-                break;
-            case 8:
-                hangDonVi = "eight";
-                break;
-            case 9:
-                hangDonVi = "nine";
-                break;
-            default:
-                break;
-        }
 
-        switch (hangChuc01){
-            case 1:
-                hangChuc = "ten";
-                break;
-            case 2:
-                hangChuc = "twenty";
-                break;
-            case 3:
-                hangChuc = "thirty";
-                break;
-            case 4:
-                hangChuc = "fourty";
-                break;
-            case 5:
-                hangChuc = "fifty";
-                break;
-            case 6:
-                hangChuc = "sixty";
-                break;
-            case 7:
-                hangChuc = "seventy";
-                break;
-            case 8:
-                hangChuc = "eighty";
-                break;
-            case 9:
-                hangChuc = "ninety";
-                break;
-            default:
-                break;
+        while (true){
+            if (cannang == 0) break;
         }
+//        switch (donVi){
+//            case 1:
+//                hangDonVi = "one";
+//                break;
+//            case 2:
+//                hangDonVi = "two";
+//                break;
+//            case 3:
+//                hangDonVi = "three";
+//                break;
+//            case 4:
+//                hangDonVi = "four";
+//                break;
+//            case 5:
+//                hangDonVi = "five";
+//                break;
+//            case 6:
+//                hangDonVi = "six";
+//                break;
+//            case 7:
+//                hangDonVi = "seven";
+//                break;
+//            case 8:
+//                hangDonVi = "eight";
+//                break;
+//            case 9:
+//                hangDonVi = "nine";
+//                break;
+//            default:
+//                break;
+//        }
+//
+//        switch (hangChuc01){
+//            case 1:
+//                hangChuc = "ten";
+//                break;
+//            case 2:
+//                hangChuc = "twenty";
+//                break;
+//            case 3:
+//                hangChuc = "thirty";
+//                break;
+//            case 4:
+//                hangChuc = "fourty";
+//                break;
+//            case 5:
+//                hangChuc = "fifty";
+//                break;
+//            case 6:
+//                hangChuc = "sixty";
+//                break;
+//            case 7:
+//                hangChuc = "seventy";
+//                break;
+//            case 8:
+//                hangChuc = "eighty";
+//                break;
+//            case 9:
+//                hangChuc = "ninety";
+//                break;
+//            default:
+//                break;
+//        }
+//
+//        switch (hangTram01){
+//            case 1:
+//                hangTram = "one hundred";
+//                break;
+//            case 2:
+//                hangTram = "two hundred";
+//                break;
+//            case 3:
+//                hangTram = "three hundred";
+//                break;
+//            case 4:
+//                hangTram = "four hundred";
+//                break;
+//            case 5:
+//                hangTram = "five hundred";
+//                break;
+//            case 6:
+//                hangTram = "six hundred";
+//                break;
+//            case 7:
+//                hangTram = "seven hundred";
+//                break;
+//            case 8:
+//                hangTram = "eight hundred";
+//                break;
+//            case 9:
+//                hangTram = " nine hundred";
+//                break;
+//            default:
+//                break;
+//        }
+//
+//        if (cannang > 0 && cannang <10){
+//            System.out.println("Can nang ban la: " + cannang);
+//        }
 
-        switch (hangTram01){
-            case 1:
-                hangTram = "one hundred";
-                break;
-            case 2:
-                hangTram = "two hundred";
-                break;
-            case 3:
-                hangTram = "three hundred";
-                break;
-            case 4:
-                hangTram = "four hundred";
-                break;
-            case 5:
-                hangTram = "five hundred";
-                break;
-            case 6:
-                hangTram = "six hundred";
-                break;
-            case 7:
-                hangTram = "seven hundred";
-                break;
-            case 8:
-                hangTram = "eight hundred";
-                break;
-            case 9:
-                hangTram = " nine hundred";
-                break;
-            default:
-                break;
-        }
-
-        if (cannang > 0 && cannang <10){
-            System.out.println("Can nang ban la: " + cannang);
-        }
-
-        // check
     }
 
 }
--- src/Buoi_03/Main.java
+++ src/Buoi_03/Main.java
@@ -3,5 +3,35 @@ package Buoi_03;
 public class Main {
     public static void main(String[] args) {
         // TODO: Implement your code here
+        // region Mang
+        // Khai bao mang
+
+//        int[] arrInt;
+//        arrInt = new int[10];
+//
+//
+//        int[] varInt = new int[10];
+//        varInt[0] = 1;
+//        varInt[1] = 2;
+//
+//        System.out.println("Length: " + arrInt.length);
+//        System.out.println("Phan tu thu " + varInt[1]);
+
+
+//        String[] stringName = new String[3];
+//        stringName[0] = "Minh";
+//        stringName[1] = "Vu";
+//        stringName[2] = "Quyen";
+//        for (int i = 0; i < stringName.length; i++) {
+//            stringName[i] = "Name" + i;
+//            System.out.println("Name: " + stringName[i]);
+//        }
+
+        int[] arrInt = new int[50];
+        int count = 0;
+        for(int i = 0; i <= arrInt.length; i++) {
+
+        }
+        //endregion
     }
 }
