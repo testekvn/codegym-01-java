bc76b2e39a85b1770d6df415485ae7ecc2c238ea
vananh <vananhgit@gmail.com>
2023-08-18 23:33:42 +0700

    Homework about OOP

--- src/Buoi_06/Animal.java
+++ src/Buoi_06/Animal.java
@@ -1,13 +1,77 @@
 package Buoi_06;
 
+import java.sql.SQLOutput;
+
 public class Animal {
-    String type, weight, height;
-    public Animal(){}
+    String type;
+    double height, weight;
+    private String setType;
 
-    public Animal(String type){}
+    public void eat(){}
+    public void play(){}
+    public void makeSound(){}
+    public Animal(){
+        type = "Cat";
+    }
+    public Animal(String type){
+        this.type = type;
+    }
+    public Animal(String type, double weight, double height){
+        this.type = type;
+        this.weight = weight;
+        this.height = height;
+    }
 
-    public Animal(String type, String weight, String height){}
+    public String getType() {
+        return this.type;
+    }
 
-//    public void
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public static void main(String[] args) {
+        Animal animalCat = new Animal();
+        Animal animalDog = new Animal("Cat");
+        Animal animalDuck = new Animal("Duck", 1.2, 50);
+
+        //region Print infor
+        System.out.println("Animal meowww: "+ animalCat.getType());
+        System.out.println("Animal " + animalDuck.getType() + " weight " + animalDuck.getWeight() + " kg, " + " height " + animalDuck.getHeight());
+
+
+        //endregion
+
+        // region Set Dog: wight & height
+        animalDog.setHeight(9);
+        animalDog.setWeight(1.3);
+        System.out.println("Dog: weight " + animalDog.getWeight() + " kg" + " & height: " + animalDog.getHeight());
+
+        // endregion
+
+
+        // region Set Cat => CAT
+        animalCat.setType("CAT");
+        System.out.println("New cat: "+ animalCat.getType());
+
+        // endregion
+
+    }
 
 }
