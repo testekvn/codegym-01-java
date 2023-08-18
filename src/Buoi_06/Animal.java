9110f36c581691f7b66813be8865e3310485f4ad
quyetpv <zmidouzkage96hy@gmail.com>
2023-08-19 03:11:11 +0700

    quyetpv: BTVN OOP Animal

--- src/Buoi_06/Animal.java
+++ src/Buoi_06/Animal.java
@@ -1,20 +1,104 @@
 package Buoi_06;
 
+import java.sql.SQLOutput;
+
 public class Animal {
     String type;
     String weight;
     String height;
 
-    //Hanh dong an
+    //Hành động ăn
     public void eat(){
 
     }
-    //Hanh dong choi
+    //Hành động chơi
     public void play(){
 
     }
-    //Hanh dong keu
-    public void makeSound(){
+    //Hành động kêu
+    public void makeSound(String type){
+        if(type == "cat"){
+            System.out.println("Đây là cat và nó kêu: meo meo !");
+        }
+        if (type == "Dog") {
+            System.out.println("Đây là dog và nó kêu: gâu gâu !");
+        }
+        if (type == "Duck") {
+            System.out.println("Đây là duck và nó kêu: cạc cạc !");
+        }
+    }
+
+    //Lấy loại Animal
+    public String getType() {
+        return this.type;
+    }
+    //Lấy chiều cao Animal
+    public String getHeight() {
+        return this.height;
+    }
+    //Lấy cân nặng Animal
+    public String getWeight() {
+        return this.weight;
+    }
+    //Cập nhật giá trị loại Animal
+    public void setType(String type) {
+        this.type = type;
+    }
+    //Cập nhật giá trị chiều cao Animal
+    public void setHeight(String height) {
+        this.height = height;
+    }
+    //Câập nhật cân nặng Animal
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    //Constructor No params
+    public Animal(){
+        type = "Dragon";
+        weight = "1000kg";
+        height = "10m";
+    }
+    //Constructor with param "type"
+    public Animal(String type){
+        this.type = type;
+    }
+    //Constructor with all params
+    public Animal(String type, String weight, String height){
+        this.type = type;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        //Khởi tạo Cat: Without param trong constructor
+        Animal Cat = new Animal();
+        System.out.println("Cat Info: "+ Cat.getType()+ "-"+ Cat.getWeight()+"-"+ Cat.getHeight());
+
+        //Khởi tạo Dog: With param "type" trong constructor
+        Animal Dog = new Animal("Red dog");
+        System.out.println("Dog Info: "+ Dog.getType()+ "-"+ Dog.getWeight()+"-"+ Dog.getHeight());
+
+        //Khởi tạo Duck: With all params
+        Animal Duck = new Animal("G Ducky", "5kg", "50 cm");
+        System.out.println("Duck Info: "+ Duck.getType()+ "-"+ Duck.getWeight()+"-"+ Duck.getHeight());
+
+        System.out.println();
+
+        //Cập nhật type cho Cat là CAT
+        Cat.setType("CAT");
+        System.out.println("NewCat Info with new type: "+ Cat.getType()+ "-"+ Cat.getWeight()+"-"+ Cat.getHeight());
+
+        //Cập nhật Weight and Height cho Dog
+        Dog.setWeight("100kg");
+        Dog.setHeight("80cm");
+        System.out.println("NewDog Info with newWeight and newHeight: "+ Dog.getType()+ "-"+ Dog.getWeight()+"-"+ Dog.getHeight());
+
+        System.out.println();
+
+        //Cập nhật phương thức makeSound
+        Cat.setType("Cat");
+        System.out.println("Cat Info: "+ Cat.getType()+ "-"+ Cat.getWeight()+"-"+ Cat.getHeight());
 
     }
 }
