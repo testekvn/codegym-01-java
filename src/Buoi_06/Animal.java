8954fea067224499151875abbe3e8800323da622
quyetpv <zmidouzkage96hy@gmail.com>
2023-08-19 02:11:55 +0700

    Add class Animal

---
+++ src/Buoi_06/Animal.java
@@ -0,0 +1,20 @@
+package Buoi_06;
+
+public class Animal {
+    String type;
+    String weight;
+    String height;
+
+    //Hanh dong an
+    public void eat(){
+
+    }
+    //Hanh dong choi
+    public void play(){
+
+    }
+    //Hanh dong keu
+    public void makeSound(){
+
+    }
+}
