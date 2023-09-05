6afc57f4412d4a3a5dd88df5ca1aa641d9e065c4
Vincent <giaduy218@gmail.com>
2023-09-05 19:31:00 +0700

    Vincent: Fix  BTVN V.Anh's Gift

--- src/Buoi_07/BirthdayVA/BirthdayGift.java
+++ src/Buoi_07/BirthdayVA/BirthdayGift.java
@@ -16,16 +16,16 @@ public class BirthdayGift {
         // Lặp từ năm 2001 đến năm 2021
         for (int year = 2001; year <= 2021; year++) {
             // Tính tuổi của V.Anh vào năm hiện tại
-            int age = year - 2001;
+            int curAge = year - 2001;
             // Nếu tuổi của V.Anh là số chẵn
-            if (age % 2 == 0 && age > 0) {
+            if (curAge > 0) {
                 // Sinh số tiền mà V.Anh được tặng sinh nhật ngẫu nhiên trong khoảng từ 1 triệu đến 10 triệu
-                int gift = random.nextInt(9000000) + 1000000;
+                int gift = random.nextInt(9000000);
                 // Cộng số tiền tặng vào số tiền hiện có của V.Anh
-                money += gift;
+                money = gift + map.get(curAge - 1);
                 // Lưu số tiền tặng vào map với key là năm và value là số tiền
-                map.put(year, gift);
             }
+            map.put(curAge, money);
         }
         // In ra thông tin số tiền mà V.Anh được tặng hàng năm
         System.out.println("Số tiền mà V.Anh được tặng hàng năm là:");
@@ -36,3 +36,4 @@ public class BirthdayGift {
         System.out.println("Số tiền hiện có của V.Anh sau 10 năm là: " + money + " đồng");
     }
 }
+
